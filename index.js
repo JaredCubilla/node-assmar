49a3b98058270b08ebd04a74c4752ad36e7b8a05
JaredCubilla <deraj.allibuc@gmail.com>
2014-10-10 11:32:05 -0400

    typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports = function(str) {
 	if (str.indexOf('asthma') !== -1 || str.indexOf('Asthma') !== -1) {
 		str = str.replace(/asthma/g, 'ass-mar');
-		str = str.replace(/asthma/g, 'Ass-mar');
+		str = str.replace(/Asthma/g, 'Ass-mar');
 	}
 	return str;
 }
