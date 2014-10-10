576f3f5818a8ccda1246865179166ea82d1e7d28
JaredCubilla <deraj.allibuc@gmail.com>
2014-10-10 11:32:14 -0400

    started testing

---
+++ test.js
@@ -0,0 +1,6 @@
+'use strict';
+var assmar = require('./');
+
+console.log(assmar('This is a test.'));
+console.log(assmar('Sucks to your asthma!'));
+console.log(assmar('Asthma is a disease that...'));
