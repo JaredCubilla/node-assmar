a00060b9274ab882e34f000ec0060d12bc26b278
JaredCubilla <deraj.allibuc@gmail.com>
2014-10-10 11:34:47 -0400

    added uppercase support

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 module.exports = function(str) {
-	if (str.indexOf('asthma') !== -1 || str.indexOf('Asthma') !== -1) {
+	if (str.indexOf('asthma') !== -1 || str.indexOf('Asthma') !== -1 || str.indexOf('ASTHMA') !== -1) {
 		str = str.replace(/asthma/g, 'ass-mar');
 		str = str.replace(/Asthma/g, 'Ass-mar');
+		str = str.replace(/ASTHMA/g, 'ASS-MAR');
 	}
 	return str;
 }
--- test.js
+++ test.js
@@ -4,3 +4,4 @@ var assmar = require('./');
 console.log(assmar('This is a test.'));
 console.log(assmar('Sucks to your asthma!'));
 console.log(assmar('Asthma is a disease that...'));
+console.log(assmar('Asthma, asthma, ASTHMA'));
